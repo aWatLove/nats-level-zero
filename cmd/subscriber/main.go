8ecad2c3889b0d7b620a728327c3a47c4726c2c2
Vladislav <watislove4u@gmail.com>
2024-02-24 21:39:55 +0400

    main some done! + .env

--- cmd/subscriber/main.go
+++ cmd/subscriber/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"github.com/aWatLove/nats-lvl-zero/internal/delivery/http"
+	"github.com/aWatLove/nats-lvl-zero/internal/repository"
+	"github.com/aWatLove/nats-lvl-zero/internal/repository/postgres"
+	"github.com/aWatLove/nats-lvl-zero/internal/service"
 	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
@@ -12,6 +17,37 @@ func main() {
 	}
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("error initializing .env variables^ %s", err.Error())
+	}
+	db, err := postgres.ConnectDB(postgres.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Username: os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialized db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+
+	// todo fill cache from db
+
+	// todo init NATS
+
+	// todo connect NATS streaming server
+
+	// todo subscribe
+
+	// init handlers
+	handlers := http.NewHandler(services)
+	// init server
+	srv := new(http.Server)
+
+	if err = srv.Run(os.Getenv("APP_PORT"), handlers.InitRoutes()); err != nil {
+		log.Fatal(err)
 	}
 
 }
