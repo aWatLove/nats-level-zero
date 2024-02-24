60e839f64ecfbe53ceeb351ab17e0a89492d38c3
Vladislav <watislove4u@gmail.com>
2024-02-24 21:37:34 +0400

    repos changed

---
+++ internal/repository/postgres/postgres.go
@@ -0,0 +1,30 @@
+package postgres
+
+import (
+	"fmt"
+	"github.com/aWatLove/nats-lvl-zero/internal/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func ConnectDB(c Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", c.Host, c.Port, c.Username, c.DBName, c.Password, c.SSLMode)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	db.AutoMigrate(&model.Delivery{}, &model.Payment{}, &model.Item{}, &model.Order{})
+
+	return db, nil
+}
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,7 +1,23 @@
 package repository
 
-import "github.com/aWatLove/nats-lvl-zero/internal/model"
+import (
+	"github.com/aWatLove/nats-lvl-zero/internal/model"
+	"github.com/aWatLove/nats-lvl-zero/internal/repository/postgres"
+	"gorm.io/gorm"
+)
 
 type Order interface {
-	Create(order model.Order)
+	Create(order model.Order) error
+	Get(uid string) (model.Order, error)
+	GetAll() ([]model.Order, error)
+}
+
+type Repository struct {
+	Order
+}
+
+func NewRepository(db *gorm.DB) *Repository {
+	return &Repository{
+		Order: postgres.NewOrderPostgres(db),
+	}
 }
